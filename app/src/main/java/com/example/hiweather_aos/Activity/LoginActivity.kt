cb0fd966d1d7594df7b7f4d96d267234a9bfccdf
eldeoddt <eldeoddt@gmail.com>
2024-06-19 01:19:11 +0900

    [FEAT] 구글 로그인 구현 #1

--- app/src/main/java/com/example/hiweather_aos/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/hiweather_aos/Activity/LoginActivity.kt
@@ -2,19 +2,30 @@ package com.example.hiweather_aos.Activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hiweather_aos.MainActivity
+import com.example.hiweather_aos.R
 import com.example.hiweather_aos.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,9 +47,48 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+        // google login
+        val requestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            Log.d("loginact","account1 : ${task.toString()}")
+            try{
+                val account = task.getResult(ApiException::class.java)
+                val crendential = GoogleAuthProvider.getCredential(account.idToken, null)
+                auth.signInWithCredential(crendential)
+                    .addOnCompleteListener(this){task ->
+                        // 로그인 성공 시 실행
+                        if(task.isSuccessful){
+                            Toast.makeText(baseContext,"구글 로그인 성공",Toast.LENGTH_SHORT).show()
+                            Log.d("loginact", "구글 로그인 성공")
+                            finish()
+                        }
+                        else{ // 로그인 실패 시 실행
+                            Toast.makeText(baseContext,"구글 로그인 실패",Toast.LENGTH_SHORT).show()
+                            Log.d("loginact", "구글 로그인 실패")
+                        }
+                    }
+            }catch (e: ApiException){ // APIException은 이미 지정된 exception말고 custom한 exception을 만들어서 쓰고 싶을때 사용
+                Toast.makeText(baseContext,"구글 로그인 Exception : ${e.printStackTrace()},${e.statusCode}",Toast.LENGTH_SHORT).show()
+                Log.d("loginact", "구글 로그인 Exception : ${e.message}, ${e.statusCode}")
+            }
+        }
+
         // 구글 로그인 로직
         binding.btnGoogleLogin.setOnClickListener {
-            // 구글 로그인 구현
+            Log.d("loginact", "클릭")
+            val gso = GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_client_id))
+                .requestEmail()
+                .build()
+            val signInIntent = GoogleSignIn.getClient(this,gso).signInIntent
+            requestLauncher.launch(signInIntent)
+        }
+
+        // 트위터 로그인 로직
+        binding.btnTwitterLogin.setOnClickListener {
+
         }
 
         // 로그아웃 버튼
@@ -94,12 +144,16 @@ class LoginActivity : AppCompatActivity() {
             binding.btnLogin.visibility = View.GONE
             binding.btnRegister.visibility = View.GONE
             binding.btnGoogleLogin.visibility = View.GONE
+            binding.btnTwitterLogin.visibility = View.GONE
             binding.btnLogout.visibility = View.VISIBLE
         } else { // 로그아웃 상태
             binding.tvUsername.visibility = View.INVISIBLE
+            binding.etPassword.visibility = View.VISIBLE
+            binding.btnLogin.visibility = View.VISIBLE
             binding.btnLogin.visibility = View.VISIBLE
             binding.btnRegister.visibility = View.VISIBLE
             binding.btnGoogleLogin.visibility = View.VISIBLE
+            binding.btnTwitterLogin.visibility = View.VISIBLE
             binding.btnLogout.visibility = View.GONE
         }
     }
