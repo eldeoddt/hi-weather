ca0490a5a42fdbb8330d85b5631a84b79981008a
eldeoddt <eldeoddt@gmail.com>
2024-06-20 04:18:41 +0900

    [FEAT] 알림 구현 추가 #1

--- app/src/main/java/com/example/hiweather_aos/NotificationHelper.kt
+++ app/src/main/java/com/example/hiweather_aos/NotificationHelper.kt
@@ -1,21 +1,30 @@
 package com.example.hiweather_aos
 
-import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 object NotificationHelper {
 
     fun sendNotification(context: Context) {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
         val builder = NotificationCompat.Builder(context, "hiweather_channel")
             .setSmallIcon(R.drawable.ic_cloudy)
-            .setContentTitle("HiWeather 알림")
+            .setContentTitle("HiWeather")
             .setContentText("정보 탭에서 시간별 기온 그래프를 확인해보세요!")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         with(NotificationManagerCompat.from(context)) {
             notify(1, builder.build())
         }
     }
+
 }
